32ff1476af0ee87b515ebcf4cd2a3220a6ef72c1
Shashank Garg <gargshashank1238@gmail.com>
2026-02-02 20:26:37 +0530

    Add files via upload

---
+++ learning/week-4-npm-basic/index.mjs
@@ -0,0 +1,6 @@
+import chalk from "chalk"; //modern import syntax
+
+
+console.log(chalk.blue('Hello, world!'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.green.underline('This is a success message.'));
